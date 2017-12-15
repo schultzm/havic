a420cc107f2155785ad23943f7aa5da3793c08dc
schultzm <dr.mark.schultz@gmail.com>
2017-12-15 14:31:36 +1100

    Fixed heatmap, added clust names

--- havtrans/plottree/ggtree.py
+++ havtrans/plottree/ggtree.py
@@ -63,9 +63,45 @@ dev.off()
 
 library(pheatmap)
 heatmap_data <- read.FASTA(basename)
-heatmap_data <- as.matrix(dist.dna(heatmap_data, model='N'))
-pheatmap(heatmap_data, show_rownames = T, show_colnames = T, display_numbers = TRUE, number_format = '%.0f', fontsize = fntsz*2, filename = paste0(basename, '_SNPdists.pdf'), width=11.69, height=8.27)
+heatmap_data <- dist.dna(heatmap_data, model='N', as.matrix = TRUE)
+re_name <- function(iname, trmatrix) {
+    if(iname %in% trmatrix[,1]){
+        row <- which(trmatrix[,1]==iname)
+        return(paste0(trmatrix[row,1], ' ', trmatrix[row,2])) 
+    }
+    else{
+        return(iname)
+    }
+}
+
+colnames(heatmap_data) <- lapply(colnames(heatmap_data),
+                                 re_name,
+                                 cluster_picks)
+rownames(heatmap_data) <- lapply(rownames(heatmap_data),
+                                 re_name,
+                                 cluster_picks)
+
+pheatmap(heatmap_data,
+         show_rownames = T,
+         show_colnames = T,
+         display_numbers = TRUE,
+         number_format = '%.0f',
+         fontsize = fntsz*2,
+         filename = paste0(basename,
+                           '_ClusterNamesPicked',
+                           nsnps,
+                           'SNPsIn',
+                           seqlen,
+                           '_SNPdists.pdf'),
+         width=11.69,
+         height=8.27)
 dev.off()
-write.csv(x=heatmap_data, file=paste0(basename, '_SNPdists.csv'), quote=FALSE)
+write.csv(x=heatmap_data, file=paste0(basename,
+                                      '_ClusterNamesPicked',
+                                      nsnps,
+                                      'SNPsIn',
+                                      seqlen,
+                                      '_SNPdists.csv'),
+          quote=FALSE)
 
 '''
