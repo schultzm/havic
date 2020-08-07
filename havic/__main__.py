03205b4ccc2e517f46ed98a1169f7f3369066e78
schultzm <dr.mark.schultz@gmail.com>
2020-08-07 18:11:25 +1000

    dev on testsuite

--- havic/__main__.py
+++ havic/__main__.py
@@ -156,30 +156,35 @@ def main():
         for software in SOFTWAREZ:
             Dependency(software, 'software').check()
     elif args.subparser_name == 'test':
-        print(f"Running test suite...")
-        from . import __parent_dir__, __test_seqs__, __test_seqs_totrim__
-        import pkg_resources
-        from .utils.pipeline_runner import Pipeline
-        test_query = [pkg_resources.resource_filename(__parent_dir__,
-                                                      __test_seqs__)]
-        test_seqs_totrim = [test_seq_totrim for test_seq_totrim in
-                            __test_seqs_totrim__]
-        detection_pipeline = Pipeline(test_query,
-                                      test_seqs_totrim,
-                                      args.subject_file,
-                                      args.redo,
-                                      args.n_snps,
-                                      args.seqlen,
-                                      args.matrixplots,
-                                      args.prefix,
-                                      args.outdir,
-                                      args.minimap2_kmer,
-                                      args.path_to_clusterpicker,
-                                      args.iqtree_threads)
-        for key, value in detection_pipeline.__dict__.items():
-            print(f"{key}: {value}\n")
-        detection_pipeline._run()
-        get_execution_time(args.outdir)
+        # print(f"Running test suite...")
+        # from . import __parent_dir__, __test_seqs__, __test_seqs_totrim__
+        # import pkg_resources
+        # from .utils.pipeline_runner import Pipeline
+        # test_query = [pkg_resources.resource_filename(__parent_dir__,
+        #                                               __test_seqs__)]
+        # test_seqs_totrim = [test_seq_totrim for test_seq_totrim in
+        #                     __test_seqs_totrim__]
+        # detection_pipeline = Pipeline(test_query,
+        #                               test_seqs_totrim,
+        #                               args.subject_file,
+        #                               args.redo,
+        #                               args.n_snps,
+        #                               args.seqlen,
+        #                               args.matrixplots,
+        #                               args.prefix,
+        #                               args.outdir,
+        #                               args.minimap2_kmer,
+        #                               args.path_to_clusterpicker,
+        #                               args.iqtree_threads)
+        # for key, value in detection_pipeline.__dict__.items():
+        #     print(f"{key}: {value}\n")
+        # detection_pipeline._run()
+        # get_execution_time(args.outdir)
+
+        import unittest
+        from .tests.test_suite import suite
+        runner = unittest.TextTestRunner(verbosity=2)
+        runner.run(suite())
 
     elif args.subparser_name == 'detect':
         from .utils.pipeline_runner import Pipeline
---
+++ havic/tests/test_suite.py
@@ -0,0 +1,18 @@
+"""
+Unit Test suite builder.
+    Copyright (C) 2020 Dr Mark B Schultz dr.mark.schultz@gmail.com
+    https://github.com/schultzm/gnb.git GNU Affero General Public License
+    <https://www.gnu.org/licenses/>.
+"""
+
+import unittest
+from ..tests.havic_tests import MergeTestCasePass
+
+
+def suite():
+    """
+    This is the test suite.
+    """
+    suite = unittest.TestSuite()
+    suite.addTest(MergeTestCasePass("versioner"))
+    return 
