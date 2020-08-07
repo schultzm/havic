13a50e80cd08e935da57149d74791b24080dde04
schultzm <dr.mark.schultz@gmail.com>
2020-08-07 17:47:24 +1000

    Starting dev on testsuite

---
+++ havic/tests/havic_tests.py
@@ -0,0 +1,28 @@
+"""
+Unit Tests.
+    Copyright (C) 2020 Dr Mark B Schultz dr.mark.schultz@gmail.com
+    https://github.com/schultzm/HAVIC.git GNU Affero General Public License
+    <https://www.gnu.org/licenses/>.
+"""
+
+import unittest
+from .. import (__parent_dir__,
+                __ref_seq__,
+                __test_seqs__,
+                __test_seqs_totrim__,
+                __version__
+                )
+import pkg_resources
+from ..utils import *
+
+
+class MergeTestCasePass(unittest.TestCase):
+    def setUp(self):
+        self.refseq    = pkg_resources.resource_filename(__parent_dir__,
+                                                         __ref_seq__)
+        self.version   = __version__
+
+    def versioner(self):
+        from .. import __version__
+        self.assertFalse(__version__ == None)
+
