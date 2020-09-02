dd507f60314a1ae5b7c43aafbfb8d4a861fff0d8
schultzm <dr.mark.schultz@gmail.com>
2020-09-02 21:48:46 +1000

    dangermouse

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 # install miniconda - see https://docs.conda.io/en/latest/miniconda.html
 export PATH=/bin:/usr/bin
-. $HOME/miniconda3/etc/profile.d/conda.sh
-conda env create -f environment.yml
+# . $HOME/miniconda3/etc/profile.d/conda.sh
 conda init
 source ~/.bashrc
+conda env create -f environment.yml
 conda activate havic_env
 echo "install.packages('BiocManager', repos='https://cloud.r-project.org')
 BiocManager::install('GenomicAlignments')
