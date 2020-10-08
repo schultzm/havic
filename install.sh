9674e122c5ad6d50a462d47f9dbed2cd581102c1
schultzm <dr.mark.schultz@gmail.com>
2020-10-08 19:33:27 +1100

    fixing install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,31 @@
 # install miniconda - see https://docs.conda.io/en/latest/miniconda.html
 # if there are troubles here, don't mix brew and conda PATH - tseemann
-conda init
-source ~/.bashrc
+# conda init
+# source ~/.bashrc
+# conda env create -f environment.yml
+# conda activate havic_env
+# echo "install.packages('Rcpp', repos='https://cloud.r-project.org')
+# install.packages('magick', repos='https://cloud.r-project.org')
+# install.packages('phytools',repos='https://cloud.r-project.org', type='source')
+# install.packages('pheatmap', repos='https://cloud.r-project.org')
+# install.packages('BiocManager', repos='https://cloud.r-project.org')
+# install.packages('colorspace', repos='https://cloud.r-project.org')
+# BiocManager::install('GenomicAlignments')
+# BiocManager::install('Biostrings')
+# BiocManager::install('Rsamtools')
+# BiocManager::install('ggtree')
+# BiocManager::install('tidyverse')" | R --no-save
+# havic test hav_amplicon
+# havic test hav_wgs
+# havic test measles_wgs
+
 conda env create -f environment.yml
+conda init bash
+source ~/.bashrc
 conda activate havic_env
-echo "install.packages('Rcpp', repos='https://cloud.r-project.org')
-install.packages('magick', repos='https://cloud.r-project.org')
-install.packages('phytools',repos='https://cloud.r-project.org', type='source')
-install.packages('pheatmap', repos='https://cloud.r-project.org')
-install.packages('BiocManager', repos='https://cloud.r-project.org')
-install.packages('colorspace', repos='https://cloud.r-project.org')
-BiocManager::install('GenomicAlignments')
-BiocManager::install('Biostrings')
-BiocManager::install('Rsamtools')
-BiocManager::install('ggtree')
-BiocManager::install('tidyverse')" | R --no-save
+conda list
 havic test hav_amplicon
 havic test hav_wgs
 havic test measles_wgs
+havic test hiv_amplicon
+
