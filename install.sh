38f2f2055a1506b7ae356141c6a139f244826ff3
schultzm <dr.mark.schultz@gmail.com>
2020-10-22 13:18:09 +1100

    testing pip install

--- install.sh
+++ install.sh
@@ -8,10 +8,12 @@ conda clean --all
 conda update conda
 conda create -n havic_env -c conda-forge python==3.9.0 r-base==4.0.3
 conda activate havic_env
-conda env update --file environment.yml --prune
+# conda env update --file environment.yml --prune
+pip install -e .
 havic version
+conda list -n havic_env
 arr=(hav_amplicon hav_wgs hav_pmc measles_wgs hiv_amplicon)
-for i in ${arr[@]}
-  do
-    echo havic test ${i}
-  done
+# for i in ${arr[@]}
+#   do
+    # echo havic test ${i}
+#   done
