742e7135481dc311baf0c8ddae0ed6875a7b71a2
schultzm <dr.mark.schultz@gmail.com>
2020-10-22 12:00:34 +1100

    dumped mamba from workflow

--- install.sh
+++ install.sh
@@ -7,10 +7,9 @@ git pull
 export PATH=${PATH}:/bin:/usr/bin:/sbin:/usr/sbin
 conda clean --all
 conda update conda
-conda install -c conda-forge mamba
-mamba create -n havic_env python==3.9.0 r-base==4.0.3
+conda create -n havic_env python==3.9.0 r-base==4.0.3
 conda activate havic_env
-mamba env update --file environment.yml --prune
+conda env update --file environment.yml --prune
 havic version
 arr=(hav_amplicon hav_wgs hav_pmc measles_wgs hiv_amplicon)
 for i in ${arr[@]}
