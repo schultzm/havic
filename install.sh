eb96e6b232e32a921710656193e37deb58772c01
schultzm <dr.mark.schultz@gmail.com>
2020-10-22 15:35:20 +1100

    moved environment.yml back to old method

--- install.sh
+++ install.sh
@@ -5,14 +5,15 @@ conda init bash
 source ~/.bashrc
 export PATH=${PATH}:/bin:/usr/bin:/sbin:/usr/sbin
 conda update conda
-conda create -n havic_env -c conda-forge python==3.9.0 r-base==4.0.3
+conda create -f environment.yml
+# conda create -n havic_env -c conda-forge python==3.9.0 r-base==4.0.3
 conda activate havic_env
-conda env update --file environment.yml --prune
-pip install -e .
+# conda env update --file environment.yml --prune
+# pip install -e .
 havic version
 conda list -n havic_env
-arr=(hav_amplicon hav_wgs hav_pmc measles_wgs hiv_amplicon)
-for i in ${arr[@]}
-  do
-    echo havic test ${i}
-  done
+# arr=(hav_amplicon hav_wgs hav_pmc measles_wgs hiv_amplicon)
+# for i in ${arr[@]}
+#   do
+    # echo havic test ${i}
+#   done
